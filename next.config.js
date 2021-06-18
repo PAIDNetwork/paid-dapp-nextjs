ae62f334a8dd81e91cc1aec933395360d08db1b9
Roberto E. Zubieta <roberto.zubieta@admios-sa.com>
2021-06-18 12:42:58 -0500

    Next 11

--- next.config.js
+++ next.config.js
@@ -7,11 +7,9 @@ module.exports = {
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: 'empty',
-        net:'empty',
-        tls:'empty'
-      }
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.tls = false;
     }
 
     return config
