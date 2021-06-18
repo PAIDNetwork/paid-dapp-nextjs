0b6325a7b9ae49bca192de108a2e99a15263fc80
Roberto Eduardo Zubieta <zubieta.roberto.e@gmail.com>
2021-06-18 16:05:21 -0500

    NextJS 11 (#26)
    * Next 11
    
    * Updated Eslint

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 const path = require('path');
 
 module.exports = {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'sass')],
   },
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: 'empty',
-        net:'empty',
-        tls:'empty'
-      }
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.tls = false;
     }
 
     return config
