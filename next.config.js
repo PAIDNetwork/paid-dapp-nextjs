e25a63164492525cc89c5c43005233c0b3a50d84
Roberto E. Zubieta <roberto.zubieta@admios-sa.com>
2021-06-18 13:05:57 -0500

    Updated Eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 
 module.exports = {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'sass')],
   },
