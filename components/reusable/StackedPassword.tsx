fe1ef2fad83277c79d95ae6d52eb83bdc84736e9
ryuhei <ryuheimat3@gmail.com>
2021-08-06 18:19:34 +0900

    update fontSize and padding for DID popup

--- components/reusable/StackedPassword.tsx
+++ components/reusable/StackedPassword.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 
 interface StackedPasswordProps {
@@ -16,41 +16,43 @@ interface StackedPasswordProps {
 
 class StackedPassword extends Component<StackedPasswordProps> {
     state = {
-        type: 'password',
-        iconEye: 'fa fa-eye'
+      type: 'password',
+      iconEye: 'fa fa-eye',
     };
 
     showHide = () => {
-        this.setState({
-            type: this.state.type === 'text' ? 'password' : 'text',
-            iconEye: this.state.iconEye === 'fa fa-eye' ? 'fa fa-eye-slash' : 'fa fa-eye'
-        });
+      this.setState({
+        type: this.state.type === 'text' ? 'password' : 'text',
+        iconEye: this.state.iconEye === 'fa fa-eye' ? 'fa fa-eye-slash' : 'fa fa-eye',
+      });
     }
 
     render() {
-        return (
-            <div className={classNames('form-group stacked-group paid-component-password', this.props.groupClassNames)}>
-                <label
-                    htmlFor={this.props.id}
-                    className={classNames('stacked-label', this.props.labelClassNames)}
-                >{this.props.label}</label>
-                <span className="paid-component-password__input">
-                    <input
-                        type={this.state.type}
-                        spellCheck="false"
-                        autoCorrect="off"
-                        name={this.props.name}
-                        autoComplete={this.props.name}
-                        placeholder={this.props.placeholder}
-                        id={this.props.id}
-                        ref={this.props.innerRef}
-                        className={classNames('form-control stacked-control', this.props.class)}
-                    />
-                    <i className={classNames('eye', this.state.iconEye)} aria-hidden="true" onClick={this.showHide}/>
-                </span>
-                {this.props.errorComponent}
-            </div>
-        );
+      return (
+        <div className={classNames('form-group stacked-group paid-component-password', this.props.groupClassNames)}>
+          <label
+            htmlFor={this.props.id}
+            className={classNames('stacked-label', this.props.labelClassNames)}
+          >
+            {this.props.label}
+          </label>
+          <span className="paid-component-password__input">
+            <input
+              type={this.state.type}
+              spellCheck="false"
+              autoCorrect="off"
+              name={this.props.name}
+              autoComplete={this.props.name}
+              placeholder={this.props.placeholder}
+              id={this.props.id}
+              ref={this.props.innerRef}
+              className={classNames('form-control stacked-control', this.props.class)}
+            />
+            <i className={classNames('eye', this.state.iconEye)} aria-hidden="true" onClick={this.showHide} />
+          </span>
+          {this.props.errorComponent}
+        </div>
+      );
     }
 }
 
