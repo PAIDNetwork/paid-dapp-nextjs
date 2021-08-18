f7588a280356a46636df9e082d8623d970f02c27
ryuhei <ryuheimat3@gmail.com>
2021-08-19 03:00:28 +0900

    update logos in sidebar

--- components/Layout/SideBar.tsx
+++ components/Layout/SideBar.tsx
@@ -1,71 +1,106 @@
-import React, { FC, useEffect, useState } from 'react'
-import { Navbar } from 'reactstrap'
-import PropTypes from 'prop-types'
-import Link from 'next/link'
-import { useDispatch, useSelector } from 'react-redux'
-import classnames from 'classnames'
-import { useWallet } from 'react-binance-wallet'
+import React, { FC, useEffect, useState } from 'react';
+import { Navbar } from 'reactstrap';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
+import classnames from 'classnames';
+import { useWallet } from 'react-binance-wallet';
 
-import setOpenMenu from '@/redux/actions/menu'
-import useContract from '../../hooks/useContract'
-import ProfileCard from '../reusable/ProfileCard'
+import setOpenMenu from '@/redux/actions/menu';
+import useContract from '../../hooks/useContract';
+import ProfileCard from '../reusable/ProfileCard';
 
 type SideBarProps = {
   routerName: string
 }
 
+const sideBarItems = [
+  {
+    link: '/agreements',
+    icon: 'standardAgreements.svg',
+    title: 'Standard Agreements',
+  },
+  {
+    link: '/smart_agreements',
+    icon: 'smartAgreement.svg',
+    title: 'Smart Agreements',
+  },
+  {
+    link: '/binance_chain',
+    icon: 'binanceSmartChain.svg',
+    title: 'BSC Bridge',
+  },
+  {
+    link: '/buy-paid',
+    icon: 'buyPaid.svg',
+    title: 'Buy PAID',
+  },
+  {
+    link: '/launchpads',
+    icon: 'launchpads.svg',
+    title: 'Launchpads',
+  },
+  {
+    link: '/staking',
+    icon: 'staking.svg',
+    title: 'Staking',
+  },
+];
+
 const SideBar: FC<SideBarProps> = ({ routerName }) => {
-  const { tokenContract, networkName } = useContract()
-  const { balance, connector } = useWallet()
-  const [toggleVisible, setToggleVisible] = useState(false)
-  const [paidToken, setPaidToken] = useState(0)
-  const isOpen = useSelector((state: any) => state.menuReducer.isOpen)
+  const { tokenContract, networkName } = useContract();
+  const {
+    balance, connector, account, reset,
+  } = useWallet();
+  const [toggleVisible, setToggleVisible] = useState(false);
+  const [paidToken, setPaidToken] = useState(0);
+  const isOpen = useSelector((state: any) => state.menuReducer.isOpen);
   const currentWallet = useSelector(
-    (state: { walletReducer: any }) => state.walletReducer.currentWallet
-  )
-  const dispatch = useDispatch()
-  const profile = useSelector((state: any) => state.profileReducer.profile)
-  const { name, did } = profile
+    (state: { walletReducer: any }) => state.walletReducer.currentWallet,
+  );
+  const dispatch = useDispatch();
+  const profile = useSelector((state: any) => state.profileReducer.profile);
+  const { name, did } = profile;
 
   useEffect(() => {
     const getToken = async () => {
       try {
-        const tokenBalance = await tokenContract.balanceOf(currentWallet)
-        setPaidToken(tokenBalance.toString() / 1e18)
+        const tokenBalance = await tokenContract.balanceOf(currentWallet);
+        setPaidToken(tokenBalance.toString() / 1e18);
       } catch (error) {
-        setPaidToken(0)
+        setPaidToken(0);
       }
-    }
+    };
 
-    if (tokenContract) getToken()
-  }, [tokenContract])
+    if (tokenContract) getToken();
+  }, [tokenContract]);
 
-  const paidSmallLogo = '/assets/icon/logoSmall.svg'
-  const collapseOut = '/assets/icon/collapse_out.png'
-  const [smallLogo, setSmallLogo] = useState(paidSmallLogo)
+  const paidSmallLogo = '/assets/icon/logoSmall.svg';
+  const collapseOut = '/assets/icon/collapse_out.png';
+  const [smallLogo, setSmallLogo] = useState(paidSmallLogo);
 
   return (
     <Navbar
       className={isOpen ? 'sidebar' : 'collapse_sidebar'}
-      color='primary'
+      color="primary"
       light
       onMouseEnter={() => {
-        setToggleVisible(true)
+        setToggleVisible(true);
       }}
       onMouseLeave={() => {
-        setToggleVisible(false)
+        setToggleVisible(false);
       }}
     >
       {isOpen ? (
-        <div className='logos mt-2'>
+        <div className="logos mt-2">
           <img
             className={
               toggleVisible
                 ? 'left logo d-block mx-auto pb-4'
                 : 'logo d-block mx-auto pb-4'
             }
-            src='/assets/images/logo.svg'
-            alt=''
+            src="/assets/images/logo.svg"
+            alt=""
           />
           <div
             className={
@@ -80,21 +115,21 @@ const SideBar: FC<SideBarProps> = ({ routerName }) => {
                 marginLeft: 'auto',
                 marginRight: 'auto',
               }}
-              src='/assets/icon/collapse_in.png'
-              alt=''
+              src="/assets/icon/collapse_in.png"
+              alt=""
               onClick={() => {
-                setSmallLogo(paidSmallLogo)
-                dispatch(setOpenMenu(!isOpen))
+                setSmallLogo(paidSmallLogo);
+                dispatch(setOpenMenu(!isOpen));
               }}
             />
           </div>
         </div>
       ) : (
-        <div className='mt-2'>
+        <div className="mt-2">
           <img
-            className='logo d-block mx-auto pb-4'
+            className="logo d-block mx-auto pb-4"
             src={smallLogo}
-            alt=''
+            alt=""
             width={40}
             onMouseEnter={() => setSmallLogo(collapseOut)}
             onMouseLeave={() => setSmallLogo(paidSmallLogo)}
@@ -103,87 +138,76 @@ const SideBar: FC<SideBarProps> = ({ routerName }) => {
         </div>
       )}
 
-      <hr />
-      <Link href='/profile'>
-        <a className={routerName === '/profile' ? 'selected' : ''}>
-          <div className='profile-card mt-2'>
-            <ProfileCard profile={profile} />
-          </div>
-        </a>
-      </Link>
-      <div className='menu mt-5'>
-        <ul className='pl-3' style={isOpen ? {} : { paddingLeft: '1.5rem' }}>
-          <Link href='/agreements'>
-            <li
-              className={routerName === '/agreements' ? 'mb-4 active' : 'mb-4'}
-            >
-              <a>
-                <img className='mr-3' src='/assets/icon/list-log.svg' alt='' />{' '}
-                Standard Agreements
-              </a>
-            </li>
-          </Link>
-          <Link href='/smart_agreements'>
-            <li
-              className={
-                routerName === '/smart_agreements' ? 'mb-4 active' : 'mb-4'
-              }
-            >
-              <a
-                className={routerName === '/smart_agreements' ? 'selected' : ''}
-              >
-                <img
-                  className='mr-3'
-                  src='/assets/icon/smartAgreement.svg'
-                  alt=''
-                />{' '}
-                Smart Agreements
-              </a>
-            </li>
-          </Link>
-          <li className={classnames('mb-4 no-cursor')}>
-            <img className='mr-3' src='/assets/icon/paid.svg' alt='' />
-            <div>
-              <div> {paidToken.toFixed(4)} PAID</div>
-              <div>
-                {((balance as any) / 1e18).toFixed(4)}{' '}
-                {connector !== 'bsc' ? 'ETH' : 'BNB'}
+      <div className="menu mt-5">
+        {
+          sideBarItems.map((item) => (
+            <Link href={item.link}>
+              <div className={`menu-item ${routerName === item.link ? 'active' : ''}`}>
+                <img src={`/assets/icon/${item.icon}`} alt="" />
+                {isOpen ? item.title : ''}
               </div>
+            </Link>
+          ))
+        }
+      </div>
+      <hr />
+
+      {account && (
+        <div className="menu">
+          <p>Wallet</p>
+          <li className="menu-item">
+            <img src="/assets/icon/paid.svg" alt="" />
+            <div>
+              <span>
+                {paidToken.toFixed(4)}
+                &nbsp;PAID
+              </span>
+              <br />
+              <span>
+                {((balance as any) / 1e18).toFixed(4)}
+                &nbsp;
+                {connector !== 'bsc' ? 'ETH' : 'BNB'}
+              </span>
             </div>
           </li>
-          <Link href='/binance_chain'>
-            <li
-              className={
-                routerName === '/binance_chain' ? 'mb-4 active' : 'mb-4'
-              }
-            >
-              <a className={routerName === '/binance_chain' ? 'selected' : ''}>
-                <img
-                  className='mr-3'
-                  src='/assets/icon/binanceSmartChain.svg'
-                  alt=''
-                />
-                Binance Smart Chain
-              </a>
-            </li>
-          </Link>
-        </ul>
-      </div>
+          <li className="menu-item">
+            <img src="/assets/icon/ethereum.svg" alt="" />
+            <div className="account">
+              <span>
+                {account.slice(0, 4)}
+                ...
+                {account.slice(38, 42)}
+              </span>
+              <img className="disconnect" src="/assets/icon/disconnect.svg" alt="" onClick={reset} />
+            </div>
+          </li>
+        </div>
+      )}
 
-      <div className='menu-bottom mt-5 pt-5'>
-        <ul className='pl-3'>
+      <div className="menu-bottom mt-5 pt-5">
+        <Link href="/profile">
+          <a className={routerName === '/profile' ? 'selected' : ''}>
+            <div className="profile-card mt-2">
+              <ProfileCard profile={profile} />
+            </div>
+          </a>
+        </Link>
+
+        <ul className="pl-3">
           <li className={classnames('mb-4 no-cursor')}>
-            <img className='mr-3' src='/assets/icon/networkBi.svg' alt='' />
-            Network: {networkName}
+            <img className="mr-3" src="/assets/icon/networkBi.svg" alt="" />
+            Network:
+            {' '}
+            {networkName}
           </li>
         </ul>
       </div>
     </Navbar>
-  )
-}
+  );
+};
 
 SideBar.propTypes = {
   routerName: PropTypes.string.isRequired,
-}
+};
 
-export default SideBar
+export default SideBar;
--- components/reusable/ProfileCard.tsx
+++ components/reusable/ProfileCard.tsx
@@ -12,22 +12,20 @@ type ProfileCardProps = {
 const ProfileCard: FC<ProfileCardProps> = ({ profile }) => {
   const isOpen = useSelector((state: any) => state.menuReducer.isOpen);
 
-  const getProfileInitials = () =>{
-    let initials = "";
-    initials += profile !== null && profile.name ? profile.name.charAt(0) : "";
-    initials += profile !== null && profile.lastName ? profile.lastName.charAt(0) : "";
+  const getProfileInitials = () => {
+    let initials = '';
+    initials += profile !== null && profile.name ? profile.name.charAt(0) : '';
+    initials += profile !== null && profile.lastName ? profile.lastName.charAt(0) : '';
     return initials;
-  }
+  };
 
   return (
     <div className={isOpen ? 'profile-component mt-4 mx-auto padding-card' : 'collapse-profile-component mt-4 mx-auto padding-card'}>
-      <div className={'profileImage'}>{getProfileInitials()}</div>
+      <div className="profileImage">{getProfileInitials()}</div>
       <div className="info d-inline-block ml-1">
         <span className="name d-block">
-          {' '}
           {profile ? `${profile?.name} ${profile?.lastName}` : 'No data yet' }
         </span>
-        <p className="network">PAID Network</p>
       </div>
     </div>
   );
